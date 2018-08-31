911a9cda1e543b1d1477311904555fa9f4ce81b0
bvaibhav95 <bvaibhav.95@gmail.com>
2018-08-31 16:12:43 +0530

    code cleaning

--- helpers/test.js
+++ helpers/test.js
@@ -2,26 +2,7 @@ const extractedFunctions = require("./extractedFunctions");
 const puppeteer = require("puppeteer");
 
 const testingFunction = async () => {
-  const browser = await puppeteer.launch({
-    args: [
-      // all related chromium flags I could find
-      "--disable-background-timer-throttling",
-      "--disable-renderer-backgrounding",
-      "--override-plugin-power-saver-for-testing=never",
-      "--disable-extensions-http-throttling"
-    ],
-    headless: false
-  });
-  const timeout = 3000000;
-  const page = await browser.newPage();
-  for (let i = 0; i < 3; i++) {
-    console.log(`navigating ${i}`);
-    await page.goto("https://google.com");
-    const input = await page.$('input[type="text"]');
-    input.type("test");
-    const submit = await page.$('input[type="submit"]');
-    await Promise.all([page.waitForNavigation(), submit.click()]);
-  }
+ 
 };
 
 module.exports = {
